b7542f1bfe6e497b9e91cf09e8da3c46e9e6b2af
carolineglass <carolineglass89@gmail.com>
2020-06-15 15:26:16 -0400

    generate button

---
+++ src/caroline.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    const countryImg = document.querySelector('.country')
+    const itemImg = document.querySelector('.item')
+
+    const getConversions = () => {
+    fetch('http://localhost:3000/conversions')
+    .then(resp => resp.json())
+    .then(data => renderCountryItem(data))}
+
+    document.addEventListener('click', e => {
+        if (e.target.className === "generate"){
+            getConversions()
+            startTimer()
+        }//end of if for generate button
+
+    })// end of event listener
+    
+   function renderCountryItem(data) {
+        const randomNumber = Math.floor(Math.random() * 100)
+        //randomNumber is 100 to match the array of 100 conversions
+        const randomInfo = data[randomNumber]
+        const createCountryImg = document.createElement("img")
+        const createItemImg = document.createElement("img")
+
+        createCountryImg.id = "generated-country-img"
+        createCountryImg.src = randomInfo["country"]["flag"]
+        countryImg.innerHTML = ""
+        countryImg.append(createCountryImg)
+
+        createItemImg.id = "generated-item-img"
+        createItemImg.src = randomInfo["item"]["img"]
+        itemImg.innerHTML = ""
+        itemImg.append(createItemImg)
+    };
+    
+    function startTimer() {
+        
+    }
+    
+
+
+})//end of DOMContentLoaded
