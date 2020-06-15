b2dc44cab63f190e470c2c5c9a8a9f9bf066fbd1
Joshua <jykim92@gmail.com>
2020-06-15 14:11:03 -0400

    html+css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 fetch('http://localhost:3000/countries')
 .then(resp => resp.json())
 .then(console.log)
+
+
+document.addEventListener('DOMContentLoaded', function(e){
+
+    const conversionsUrl = ""
+
+})
