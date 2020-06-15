a85b13e903375c22abfc0ceca39696ae10627336
Joshua <jykim92@gmail.com>
2020-06-15 15:26:41 -0400

    josh commit

--- src/josh.js
+++ src/josh.js
@@ -6,5 +6,16 @@ fetch('http://localhost:3000/countries')
 document.addEventListener('DOMContentLoaded', function(e){
 
     const conversionsUrl = ""
+    const counter = document.getElementById('counter')
+
+    let decrementCounter = () => {
+        let number = parseInt(counter.innerText)
+        if (number > 0){
+            number -- 
+        counter.innerText = number
+        } // else if when number = 0 to throw an error to user time is up 
+    }
+
+    let timer = setInterval (decrementCounter, 1000);
 
 })
